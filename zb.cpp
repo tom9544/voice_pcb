cafb48e22dcec17c28026d973c1de2d4605a9a45
ziboz <ziboz@xilinx.com>
2021-12-08 10:29:34 +0800

    new file: zb.h & modify zb.cpp

--- zb.cpp
+++ zb.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "zb.h"
 int zb_func()
 {
-  
+  kernel_calcu(); 
   //printf
   return 1;
 }
---
+++ zb.h
@@ -0,0 +1,9 @@
+#ifndef __ZB.H__
+#define __ZB.H__
+
+int kernel_calcu()
+{
+
+
+  return 0;
+}
